cc3890f89886f9b1fbf4852b0841db7d969cfebf
Grashev.Vladislav <lord00111@gmail.com>
2021-03-15 18:45:55 +0300

    замена list на set

--- src/main/kotlin/HeadPhysician.kt
+++ src/main/kotlin/HeadPhysician.kt
@@ -1,11 +1,10 @@
 class HeadPhysician(name: String, workplace: String) : Doctor(name, workplace), Mentor {
-    private var list: ArrayList<Worker> = arrayListOf()
-
+    private var set: HashSet<Worker> = hashSetOf()
     override fun teaches(worker: Worker) {
 
         if (worker is Doctor) {// проверка являеться ли обучаемый поваром
-            if (!list.contains(worker)) {//проверка обучался воркер у него
-                list.add(worker)// проверка являеться ли обучаемый и ментор одним человеком
+            if (!set.contains(worker)) {//проверка обучался воркер у него
+                set.add(worker)// проверка являеться ли обучаемый и ментор одним человеком
                 if (worker == this) println("Самого себя обучить невозможно")
                 else println("$name обучил еще одного врача")
             } else println("${worker.name} уже обучался у этого главврача ")
@@ -13,11 +12,11 @@ class HeadPhysician(name: String, workplace: String) : Doctor(name, workplace), 
     }
 
     override fun numberOfStudents() {
-        println("${list.size} - количество врачей которыч обучил $name")
+        println("${set.size} - количество врачей которыч обучил $name")
     }
 
     override fun numberOfStudents(worker: Worker) {
-        if (list.contains(worker)) println("${worker.name} обучался у этого главврача")
+        if (set.contains(worker)) println("${worker.name} обучался у этого главврача")
         else println("${worker.name} не обучался у этого главврача")
     }
 }
