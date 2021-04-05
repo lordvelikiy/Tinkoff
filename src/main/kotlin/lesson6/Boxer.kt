63f25c52972766b580d0655a53661fc779066983
Grashev.Vladislav <lord00111@gmail.com>
2021-04-05 14:01:21 +0300

    homework

--- src/main/kotlin/lesson6/Boxer.kt
+++ src/main/kotlin/lesson6/Boxer.kt
@@ -1,13 +1,5 @@
 package lesson6
 
-
-//4)Создать класс для тестирования DSL c не менее чем 2 полями и 1 методом
-// с входными/выходными параметрами. Для класса создать одноименную функцию,
-// которая будет принимать в качестве параметра лямбду вида
-// block: ClassName.() -> Unit и будет возвращать новый экземпляр класса,
-// с примененной к нему лямбдой. Написать тест с использованием mock’a,
-// настроить mock, чтобы для каждого свойства и метода возвращалось определённое значение.
-
 data class Boxer(
     var id: Int = 0,
     var speed: Int = 0,
@@ -21,8 +13,8 @@ fun boxer(block: Boxer.() -> Unit): Boxer =
     Boxer().apply(block)
 
 fun main() {
-    val boxer = Boxer(3,16,42)
-    val boxer2 = Boxer(1,15,21)
-      println(boxer.willWin(boxer2))
-      println(boxer2.willWin(boxer))
+    val boxer = Boxer(3, 16, 42)
+    val boxer2 = Boxer(1, 15, 21)
+    println(boxer.willWin(boxer2))
+    println(boxer2.willWin(boxer))
 }
--- src/main/kotlin/lesson6/Human.kt
+++ src/main/kotlin/lesson6/Human.kt
@@ -1,36 +1,14 @@
 package lesson6
 
-//2)Создать класс с 2 методами, один возвращает булевое значение, другой
-// – отличный от булевого тип. Оба методы должны иметь минимум 1 входной параметр,
-// в зависимости от которого формируется логика каждого метода.
 data class Human(
     val name: String,
     val age: Int
 ) {
-    //одинаковые Имена
     fun identicalNames(human: Human) = this.name == human.name
 
-    //осталось состариться
     fun leftToAge(needAge: Int): String {
         if (age >= needAge)
             return "age normal"
         return "need another ${needAge - age} years"
     }
-}
-
-fun main() {
-    val a = Human("gogi", 23)
-    val b = Human("kola", 13)
-    val b1 = Human("Ford", 18)
-    val b2 = Human("lotus", 16)
-    val b3 = Human("Zara", 44)
-
-
-
-    println(a.identicalNames(b))
-    //false
-    println(a.leftToAge(17))
-    //age normal
-    println(b.leftToAge(17))
-    //need another 4 years
 }
